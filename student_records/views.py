d73e4e1294b3597cced0f98af392bbeeb401a3d8
himanshu <himanshuaggarwal.0011@gmail.com>
2025-05-05 15:51:05 +0530

    here i have made changes in view, model, serializer

--- student_records/views.py
+++ student_records/views.py
@@ -1,62 +1,47 @@
-from django.shortcuts import render
-
-from .models import Student,Course,Teacher
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import json
-from .serializers import StudentSerializer,CourseSerializer,TeacherSerializer
+from rest_framework import status
+from .models import Student, Course, Teacher
+from .serializers import StudentSerializer, CourseSerializer, TeacherSerializer
 
-# API to get the list of students
-  
-@api_view(['GET'])  # function-based GET with serializer
-def Student_Details(request):
-    students_list = Student.objects.all()
-    serializer = StudentSerializer(students_list, many=True)
-    return Response(serializer.data)
+# Student List View
+class StudentListView(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
 
-# API to post the student details
-class StudentDetailsPost(APIView):  # class-based POST with serializer
     def post(self, request):
-        serializer = StudentSerializer(data=request.data) # This create a new instances of StudentSerializer with the data recieved from client
-        if serializer.is_valid(): # it checks the condition if the data recieved is valid according to the of rules of StudentSerializer if yes it store the data 
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
             student = serializer.save()
-            return Response(StudentSerializer(student).data, status=201)
-        return Response(serializer.errors, status=400) # here if rules are not follwed it showes the error 404   
-    
-# API to get the list of courses  
- 
-@api_view(['GET'])  # function-based GET
-def Course_Details(request):
-    course_list = Course.objects.all() 
-    serializer = CourseSerializer(course_list, many=True) # call CourseSerializer and show the response in jason format
-    return Response(serializer.data)
+            return Response(StudentSerializer(student).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# API to post the courses  
+# Course List View
+class CourseListView(APIView):
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
 
-class CoursetDetailsPost(APIView):  # class-based POST with serializer
     def post(self, request):
-        serializer = CourseSerializer(data=request.data) # This create a new instances of CourseSerializer with the data recieved from client
-        if serializer.is_valid(): # it checks the condition if the data recieved is valid according to the of rules of CourseSerializer if yes it store the data 
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
             course = serializer.save()
-            return Response(CourseSerializer(course).data, status=201) # if serializer rules are followed show status=201 and data post
-        return Response(serializer.errors, status=400) # here if rules are not follwed it showes the error 404  
-     
-# API to get the list of teachers 
+            return Response(CourseSerializer(course).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])  # function-based GET
-def Teacher_Details(request):
-    teacher_list = Teacher.objects.all()
-    serializer = TeacherSerializer(teacher_list, many=True)
-    return Response(serializer.data)
+# Teacher List View
+class TeacherListView(APIView):
+    def get(self, request):
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data)
 
-# API to post teacher_details 
-
-class TeacherDetailsPost(APIView):  # class-based POST with serializer
     def post(self, request):
-        serializer = TeacherSerializer(data=request.data) # This create a new instances of TeacherSerializer with the data recieved from client
-        if serializer.is_valid():# it checks the condition if the data recieved is valid according to the of rules of TeacherSerializer if yes it store the data 
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
             teacher = serializer.save()
-            return Response(TeacherSerializer(teacher).data, status=201) # if serializer rules are followed show status=201 and data post
-        return Response(serializer.errors, status=400) # here if rules are not follwed it showes the error 404  
-     
+            return Response(TeacherSerializer(teacher).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
