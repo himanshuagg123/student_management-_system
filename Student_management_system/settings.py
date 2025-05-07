4f979755f36ba6d42634879f7b229337bb9b3d0c
himanshu <himanshuaggarwal.0011@gmail.com>
2025-05-07 16:19:02 +0530

    commit added oostgre sql

--- Student_management_system/settings.py
+++ Student_management_system/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'Student_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'table1',
+        'USER': 'himanshu',
+        'PASSWORD': 'Welcome@001',
+        'HOST': 'localhost',
+        'PORT': '5432', }
 }
 
 
